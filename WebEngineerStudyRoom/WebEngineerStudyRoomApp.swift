dc09719b077f2339e777cf090dd978d14107df3b
ame <shachi10969@gmail.com>
2022-11-11 06:33:47 +0900

    Initial Commit

---
+++ WebEngineerStudyRoom/WebEngineerStudyRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebEngineerStudyRoomApp.swift
+//  WebEngineerStudyRoom
+//
+//  Created by t&a on 2022/11/11.
+//
+
+import SwiftUI
+
+@main
+struct WebEngineerStudyRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
